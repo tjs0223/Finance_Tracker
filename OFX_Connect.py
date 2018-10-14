3b49bfc37430be76f0e24b91c039153f314025e4
tjs0223 <31713166+tjs0223@users.noreply.github.com>
2018-10-14 11:44:29 -0400

    Atom Test

---
+++ OFX_Connect.py
@@ -0,0 +1,16 @@
+# Figure out how to connect OFX with acct in secure way
+# especially if this is on GH
+
+
+'''
+example code to help remember syntax for python. tabs and colons 
+def main():
+
+    i = 2
+    while (i<5):
+        print("TEST")
+        i +=1
+
+
+
+main()'''
