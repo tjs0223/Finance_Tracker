a4c466c40bf1095c7106982522d8096740e11163
tjs0223 <31713166+tjs0223@users.noreply.github.com>
2018-11-12 15:37:05 -0500

    updated webscrape

--- web_connect.py
+++ web_connect.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import certifi
+import urllib3
 
+# use mint API from github - mimic for webscraping using click() from selenium(?)
+# https://github.com/mrooney/mintapi/blob/master/mintapi/api.py
 def main():
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
     url = 'https://verified.capitalone.com/sic-ui/#/esignin'
-    response = requests.get(url, auth=('user','pw'), verify = False)
+    http.request('GET', url)
+    response = requests.get(url, auth=('user','pw'), verify = True)
     page = BeautifulSoup(response.text, "lxml")
     print(page)
-    '''
+'''
 payload = {
     'barcode': 'your user name/login',
     'telephone_primary': 'your password',
